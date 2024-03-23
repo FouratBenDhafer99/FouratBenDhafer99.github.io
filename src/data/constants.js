aada65f357e9eaf1f3d6b354a0428ddc1d38c1c0
Fourat Ben Dhafer <fouratbendhafer99@gmail.com>
2024-03-23 05:15:39 +0100

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,295 @@
+export const Bio = {
+    name: "Fourat Ben Dhafer",
+    roles: [
+        "Software engineer",
+        "Problem Solver",
+        "Student",
+    ],
+    description:
+        "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/FouratBenDhafer99",
+    resume: "https://drive.google.com/file/d/15tZbd4FvhNJ5J6-_zH34ezf5MTGPSjH3/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/fourat-ben-dhafer/",
+    twitter: "https://twitter.com/Fourat_B_Dhafer",
+    insta: "https://www.instagram.com/fourat_ben_dhafer/",
+    facebook: "https://www.facebook.com/fourat.bendhafer",
+};
+
+export const skills = [
+    {
+        title: "Programming Languages",
+        skills: [
+            {
+                name: "HTML",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+            },
+            {
+                name: "CSS",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+            },
+            {
+                name: "JavaScript",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg",
+            },
+            {
+                name: "PHP",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+            },
+            {
+                name: "Java",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+            },
+            {
+                name: "Solidity",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/solidity/solidity-original.svg",
+            },
+        ],
+    },
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "React Js",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+            },
+            {
+                name: "Angular",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/angular/angular-original.svg",
+            },
+            {
+                name: "Bootstrap",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+            },
+            {
+                name: "Tailwind",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+            },
+            {
+                name: "MaterialUI",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+            },
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Node Js",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+            },
+            {
+                name: "Symfony",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/symfony/symfony-original.svg",
+            },
+            {
+                name: "Laravel",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-original.svg",
+            },
+            {
+                name: "Spring-boot",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg",
+            },
+        ],
+    },
+    {
+        title: "Database",
+        skills: [
+            {
+                name: "MongoDB",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+            },
+            {
+                name: "MySQL",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+            },
+
+
+        ],
+    },
+    {
+        title: "Others",
+        skills: [
+            {
+                name: "Git",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+            },
+            {
+                name: "Docker",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
+            },
+            {
+                name: "Nginx",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg",
+            },
+        ],
+    },
+];
+
+export const experiences = [
+
+    {
+        id: 0,
+        img: require('../images/GIC.png'),
+        role: "Fullstack Developer Intern",
+        company: "El Gazala Innovation Center",
+        date: "March 2021 - June 2021",
+        desc: "I developed a DApp for tracing milk using Blockchain technology.",
+        skills: [
+            "HTML",
+            "CSS",
+            "ReactJS",
+            "Spring-boot",
+            "Solidity",
+        ],
+        //doc: require('../images/FSI.jpg'),
+    },
+    {
+        id: 1,
+        img: require('../images/technoGM.png'),
+        role: "Front-end developer intern",
+        company: "TechnoGM",
+        date: "July 2022 - August 2022",
+        desc: "I developed and tested multiple interfaces for the web application.",
+        skills: [
+            "HTML",
+            "CSS",
+            "ReactJS",
+            "TailwindCSS",
+            "MaterialUI",
+
+        ],
+        //doc: require('../images/SI-II.jpg'),
+    },
+    {
+        id: 2,
+        img: require('../images/ats-nobg.png'),
+        role: "Software engineer",
+        company: "HMI Engineering Services",
+        date: "June 2023 - Present",
+        desc: "Learning and working closely with experts on Olympiqs, a challenge and Hackathon platform",
+        skills: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "TypeScript",
+            "Symfony",
+            "MongoDB",
+        ],
+        //doc: require('../images/SI-I.png'),
+    },
+];
+
+export const education = [
+    {
+        id: 0,
+        img: require('../images/canal.jpg'),
+        school: "El Canal High-school",
+        date: "September 2015 - July 2018",
+        degree: "Baccalaureate In Computer Science",
+    },
+    {
+        id: 1,
+        img: require('../images/FSB.png'),
+        school: "Bizerte's Faculty Of Sciences",
+        date: "September 2018 - July 2021",
+        degree: "Bachelor Degree In Computer Sciences ",
+    },
+    {
+        id: 2,
+        img: require('../images/esprit.jpg'),
+        school: "The Private Higher School of Engineering and Technology (ESPRIT)",
+        date: "September 2021 - Present",
+        degree: "Software Engineering",
+    },
+];
+
+export const projects = [
+    {
+        id: 0,
+        title: "Minassa",
+        date: "February 2023 - June 2023",
+        description:
+            "A community engagement platform that links citizens to lawmakers, ensuring easier decision making.",
+        image:
+            require('../images/minassa.png'),
+        tags: ["HTML", "CSS", "ReactJs", "NodeJs", "ExpressJs", "MongoDB", "Nginx", "Solidity", "Blockchain", "Scrum", "Git", "Jira",],
+        category: "web app",
+        webapp: "https://minassa-oz.tn/",
+        video: "https://youtu.be/E28sTyxNnC0?si=16GG5m_PXB0LtBAm"
+    },
+    {
+        id: 1,
+        title: "Inclusify",
+        date: "September 2023 – November 2023",
+        description:
+            "A social platform and job board specifically designed for people with reduced mobility.",
+        image: require('../images/Inclusify.png'),
+        tags: ["HTML", "CSS", "ReactJs", "Spring-boot", "Laravel", "SparkQL", "Devops", "Docker", "Microservices", "Git",],
+        category: "web app",
+        github: "",
+        webapp: "",
+    },
+    {
+        id: 5,
+        title: "Livmo",
+        date: "July 2022 - August 2022",
+        description:
+            "An all experience planing platform that helps both tourists and services providers.",
+        image: require('../images/livmo.png'),
+        tags: ["HTML", "CSS", "MaterialUI", "TailwindCSS", "ReactJs", "NodeJs", "Git",],
+        category: "web app",
+        webapp: "https://livmotestt.web.app/",
+    },
+    {
+        id: 2,
+        title: "Kaddem",
+        date: "Nov 2023 - Dec 2023",
+        description:
+            "A web application that assists teachers observing their students improvements.",
+        image: require('../images/kaddem.png'),
+        tags: ["TypeScript", "Angular", "Spring-boot", "SQL", "Git"],
+        category: "machine learning",
+        github: "https://github.com/FouratBenDhafer99/kaddem-project",
+    },
+
+    {
+        id: 3,
+        title: "Infernal Games",
+        date: "February 2022 - June 2022",
+        description:
+            "Developed a gaming platform (Website, desktop and mobile). My primary tasks were live streaming and newsletter.",
+        image:
+            require('../images/infenralgames.png'),
+        tags: ["HTML", "CSS", "JavaScript", "Symfony", "Twig", "Java", "JavaFx", "CodeNameOne", "Scrum", "Git", "Nginx"],
+        category: "web app",
+    },
+    {
+        id: 4,
+        title: "Milk Trust",
+        date: "March 2021 – June 2021",
+        description:
+            "Developed with my partner a web application for tracing milk with the Blockchain technology (dApp interacts with Ethereum contract).\n" +
+            "We succeeded to introduce Blockchain to our university as they have started teaching it the following year using our project as reference.",
+        image:
+            require('../images/milktrust.png'),
+        tags: ["HTML", "CSS", "ReactJs", "Bootstrap", "Spring-boot", "SQL", "Solidity", "Truffle", "Ganache", "RUP"],
+        category: "web app",
+    },
+];
